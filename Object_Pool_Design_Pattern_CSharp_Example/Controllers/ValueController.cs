ca43740f022fade70c2555d9c5d5e811c719178a
Dilan ÖZER <dilanozer@Dilans-MacBook-Air.local>
2024-07-30 22:02:28 +0300

    object pool design pattern - aspnetcore ile örneklendirme

---
+++ Object_Pool_Design_Pattern_CSharp_Example/Controllers/ValueController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ObjectPool;
+using Object_Pool_Design_Pattern_CSharp_Example.Classes;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Object_Pool_Design_Pattern_CSharp_Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        readonly ObjectPool<X> _pool;
+
+        public ValueController(ObjectPool<X> pool)
+        {
+            _pool = pool;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Get1()
+        {
+            var x1 = _pool.Get();
+            x1.Count++;
+            _pool.Return(x1);
+            return Ok(x1.Count);
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Get2()
+        {
+            var x2 = _pool.Get();
+            x2.Count++;
+            _pool.Return(x2);
+            return Ok(x2.Count);
+        }
+    }
+}
+
---
+++ Object_Pool_Design_Pattern_CSharp_Example/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.Extensions.ObjectPool;
+using Object_Pool_Design_Pattern_CSharp_Example.Classes;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+builder.Services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+builder.Services.AddSingleton(p =>
+{
+    var provider = p.GetService<ObjectPoolProvider>();
+    return provider.Create<X>();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+// Enable middleware to serve generated Swagger as a JSON endpoint.
+app.UseSwagger();
+
+// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+// specifying the Swagger JSON endpoint.
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; // Swagger UI'nin ana sayfada açılmasını istiyorsanız, RoutePrefix'i boş string olarak ayarlayın.
+});
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
