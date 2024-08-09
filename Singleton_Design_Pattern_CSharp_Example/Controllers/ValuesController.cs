3c09c68bb3bf611d1a47ae749ee7b1f91e99fd24
Dilan ÖZER <dilanozer@Dilans-MacBook-Air.local>
2024-08-09 21:07:37 +0300

    singleton design pattern - aspnetcore ile orneklendirme

---
+++ Singleton_Design_Pattern_CSharp_Example/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Singleton_Design_Pattern_CSharp_Example.Services;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Singleton_Design_Pattern_CSharp_Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult X()
+        {
+            DatabaseService databaseService = DatabaseService.GetInstance;
+            databaseService.Connection();
+            databaseService.Disconnect();
+            return Ok(databaseService);
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Y()
+        {
+            DatabaseService databaseService = DatabaseService.GetInstance;
+            databaseService.Connection();
+            databaseService.Disconnect();
+            return Ok(databaseService);
+        }
+    }
+}
+
